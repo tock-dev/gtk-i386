b3b1e3f4055ba6ab2a9fe8cde2185c1bef70eff3
Matthias Clasen <mclasen@redhat.com>
2025-06-07 20:25:17 -0400

    gtk: Make new snapshot border api public

--- gtk/gtksnapshot.c
+++ gtk/gtksnapshot.c
@@ -3304,7 +3304,7 @@ gtk_snapshot_append_border (GtkSnapshot          *snapshot,
     gdk_color_finish (&color[i]);
 }
 
-/*< private >
+/**
  * gtk_snapshot_add_border:
  * @snapshot: a `GtkSnapshot`
  * @outline: the outline of the border
@@ -3316,6 +3316,8 @@ gtk_snapshot_append_border (GtkSnapshot          *snapshot,
  * Appends a stroked border rectangle inside the given @outline.
  *
  * The four sides of the border can have different widths and colors.
+ *
+ * Since: 4.20
  */
 void
 gtk_snapshot_add_border (GtkSnapshot          *snapshot,
